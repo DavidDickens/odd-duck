be0e800d3c00d50332f251a380c48805b82c6518
DavidDickens <gamrdave1@gmail.com>
2023-03-23 04:20:43 +0000

    trying to unbreak it all

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-'use strict';
+//'use strict';
 
 const state = [];
-let roundsOfVoting = 25;
-let chartObj = null;
+let maxRounds = 5;
+/* let chartObj = null; */
 
 let trackerEl = document.getElementById('votingTracker');
 /* let containerEl = document.getElementById('productImages'); */
@@ -12,81 +12,74 @@ let pEls = document.querySelectorAll('#productImages .container p');
 let showResultsBtn = document.getElementById('showResultBtn');
 showResultsBtn.style.display = 'none';
 
-let resultsTable = document.getElementById('resultsContainer');
+/* let resultsTable = document.getElementById('resultsContainer'); */
 
+let canvas = document.getElementById('canvas');
 
-
-function Image(name, source) {
+function CreateProduct(name, source, alt) {
   this.name = name;
   this.timesClicked = 0;
   this.timesShown = 0;
   this.source = source;
+  this.altTxt = alt;
+  state.push(this);
 }
 
-state.push(new Image('bag', 'img/bag.jpg'));
-state.push(new Image('banana', 'img/banana.jpg'));
-state.push(new Image('bathroom', 'img/bathroom.jpg'));
-state.push(new Image('boots', 'img/boots.jpg'));
-state.push(new Image('breakfast', 'img/breakfast.jpg'));
-state.push(new Image('bubblegum', 'img/bubblegum.jpg'));
-state.push(new Image('chair', 'img/chair.jpg'));
-state.push(new Image('cthulhu', 'img/cthulhu.jpg'));
-state.push(new Image('dog duck', 'img/dog-duck.jpg'));
-state.push(new Image('dragon', 'img/dragon.jpg'));
-state.push(new Image('pen', 'img/pen.jpg'));
-state.push(new Image('pet sweep', 'img/pet-sweep.jpg'));
-state.push(new Image('scissors', 'img/scissors.jpg'));
-state.push(new Image('shark', 'img/shark.jpg'));
-state.push(new Image('sweep', 'img/sweep.png'));
-state.push(new Image('tauntaun', 'img/tauntaun.jpg'));
-state.push(new Image('unicorn', 'img/unicorn.jpg'));
-state.push(new Image('water can', 'img/water-can.jpg'));
-state.push(new Image('wine glass', 'img/wine-glass.jpg'));
+new CreateProduct('bag', 'img/bag.jpg', 'Bag');
+new CreateProduct('banana', 'img/banana.jpg', 'Banana');
+new CreateProduct('bathroom', 'img/bathroom.jpg', 'Bathroom');
+new CreateProduct('boots', 'img/boots.jpg', 'Boots');
+new CreateProduct('breakfast', 'img/breakfast.jpg', 'Breakfast');
+new CreateProduct('bubblegum', 'img/bubblegum.jpg', 'Bubble gum');
+new CreateProduct('chair', '.img/chair.jpg', 'Chair');
+new CreateProduct('cthulhu', '.img/cthulhu.jpg', 'Cthulhu');
+new CreateProduct('dog-duck', 'img/dog-duck.jpg', 'Dog duck');
+new CreateProduct('dragon', 'img/dragon.jpg', 'Dragon meat');
+new CreateProduct('pen', 'img/pen.jpg', 'Pen');
+new CreateProduct('pet-sweep', 'img/pet-sweep.jpg', 'Pet sweep');
+new CreateProduct('scissors', 'img/scissors.jpg', 'Pizza scissorss');
+new CreateProduct('shark', 'img/shark.jpg', 'Shark');
+new CreateProduct('sweep', 'img/sweep.png', 'Sweep');
+new CreateProduct('tauntaun', 'img/tauntaun.jpg', 'Tauntaun');
+new CreateProduct('unicorn', '.img/unicorn.jpg', 'Unicorn meat');
+new CreateProduct('water-can', 'img/water-can.jpg', 'Watering can');
+new CreateProduct('wine-glass', 'img/wine-glass.jpg', 'Wine glass');
 
-console.log("CURRENTLY RENDERED IMAGES", imgEls);
+/* console.log("CURRENTLY RENDERED IMAGES", imgEls);
 
 console.log('CURRENT STATE', state);
-
+ */
 renderImages();
 
 function randomNum() {
   return Math.floor(Math.random() * state.length);
 }
 
+// put inside renfunction renderImages(){
 function renderImages() {
+  canvas.style.display = 'none';
+  let prevImgs = [];
 
-  let product1 = state[randomNum()];
-  let product2 = state[randomNum()];
-  let product3 = state[randomNum()];
+  for (let i = 0; i < 3; i++) {
+    let product = state[randomNum()];
 
-  if (product1.name === product2.name || product1.name === product3.name) {
-    product1 = state[randomNum()]
-  } else if (product2.name === product1.name || product2.name === product3.name) {
-    product2 = state[randomNum()]
-  } else if (product3.name === product1.name || product3.name === product2.name) {
-    product3 = state[randomNum()]
+    // Check for duplicate product names
+    while (product.name === imgEls[0].id || product.name === imgEls[1].id || product.name === imgEls[2].id || prevImgs.includes(product.name)) {
+      product = state[randomNum()];
+    }
+
+    prevImgs.push(product.name);
+
+    imgEls[i].src = product.source;
+    imgEls[i].id = product.name;
+    imgEls[i].alt = product.altTxt;
+    product.timesShown += 1;
+    product.productClicked += 1;
+
+    pEls[i].textContent = `${product.name}`;
   }
 
-  imgEls[0].src = product1.source;
-  imgEls[0].id = product1.name;
-  product1.timesShown += 1;
-
-
-  imgEls[1].src = product2.source;
-  imgEls[1].id = product2.name;
-  product2.timesShown += 1;
-
-  imgEls[2].src = product3.source;
-  imgEls[2].id = product3.name;
-  product3.timesShown += 1;
-
-  pEls[0].textContent = `${product1.name}`;
-  pEls[1].textContent = `${product2.name}`;
-  pEls[2].textContent = `${product3.name}`;
-
 }
-
-
 function handleClick(event) {
   let productClicked = event.target.id;
 
@@ -96,78 +89,48 @@ function handleClick(event) {
     }
   });
 
-  if (roundsOfVoting - 1) {
+  if (maxRounds - 1) {
     renderImages();
-    roundsOfVoting--;
-
+    maxRounds--;
   } else {
     trackerEl.removeEventListener('click', handleClick);
-    trackerEl.style.display = 'none';
+    // trackerEl.style.display = 'none';
     showResultsBtn.style.display = 'block';
+
+
   }
+
+  storeData();
 }
-
-trackerEl.addEventListener('click', handleClick)
-
-
-
-function displayResults() {
-
-  resultsTable.style.display = 'block';
-
-  let tbody = document.createElement('tbody');
-  resultsTable.appendChild(tbody);
-
-
-  resultsTable.appendChild
-  state.forEach(item => {
-    console.log(item.name, item.timesClicked, item.timesShown, item.source);
-    // containerEl.innerHTML ='';
-    let trow = document.createElement('tr');
-    tbody.appendChild(trow);
-    trow.innerHTML = `
-    <td><img src="${item.source}"/></td>
-    <td>${item.name}</td>
-    <td>${item.timesClicked}</td>
-    <td>${item.timesShown}</td>
-    `;
-    tbody.appendChild(trow);
-  });
-
-
-}
-
-showResultsBtn.addEventListener('click', displayResults);
-
-const canvasEl = document.getElementById('chart');
 
 function drawChart() {
   let labels = [];
-  let timesShownValues = [];
-  let timesClickedValues = [];
-  // look at all object inside of state
-  state.forEach(product => {
-    // labels??
-    labels.push(product.name);
-    // dataset values??
-    timesShownValues.push(product.timesShown);
-    timesClickedValues.push(product.timesClicked);
+  let timesShownVal = [];
+  let timesClickedVal = [];
+
+  state.forEach(item => {
+    labels.push(item.name);
+    timesClickedVal.push(item.timesClicked);
+    timesShownVal.push(item.timesShown);
+
   });
 
+  canvas.style.display = 'block';
 
-  return new Chart(canvasEl, {
+
+  return new Chart(canvas, {
     type: 'bar',
     data: {
-      labels: labels, // how can we get the names of our goats??
+      labels: labels,
       datasets: [{
-        label: 'Times Shown',
-        data: timesShownValues, // where does this data live?
+        label: 'Times Clicked',
+        data: timesClickedVal,
         borderWidth: 1
       }, {
-        label: 'Times Clicked',
-        data: timesClickedValues, // where does this data live?
+        label: 'Times Shown',
+        data: timesShownVal,
         borderWidth: 1
-      }], // do we have more than 1 dataset?
+      }],
     },
     options: {
       scales: {
@@ -179,15 +142,27 @@ function drawChart() {
   });
 }
 
-let buttonEl = document.getElementById('update-chart');
-buttonEl.addEventListener('click', function () {
-  updateChart([1, 2, 3, 4,5, 6, 7, 8, 9]);
-});
- 
-
-function updateChart(data) {
-  console.log("Chart up to date?", chartObj.data);
-  chartObj.data.datasets[0].data = data;
-  chartObj.update();
+function displayResults() {
+  drawChart();
 }
-// put inside render data function
+
+function storeData() {
+  let data = JSON.stringify(state);
+  localStorage.setItem('productData', data);
+}
+
+function readData() {
+  let data = localStorage.getItem('productData');
+
+  if (data) {
+    state = JSON.parse(data);
+  } else {
+    storeData();
+  }
+}
+
+readData();
+
+trackerEl.addEventListener('click', handleClick);
+showResultsBtn.addEventListener('click', displayResults);
+
